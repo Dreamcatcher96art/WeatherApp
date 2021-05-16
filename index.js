98b7cb8b3f8d70ab1eaaa79a5ed637a0ae7b1917
Dreamcatcher96art <dreamcatcher.art96@gmail.com>
2021-05-16 16:23:50 +0200

    UpdateTime

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+function formatDate (timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours <10){
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10){
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+}
+
+
+
 function displayTemperature(response){
     console.log(response.data)
 let temperatureElement = document.querySelector("#temperature");
@@ -12,6 +29,8 @@ let pressureElement = document.querySelector("#press");
 pressureElement.innerHTML = Math.round(response.data.main.pressure);
 let windSpeedElement = document.querySelector("#winds");
 windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+let timeElement = document.querySelector("#time");
+timeElement.innerHTML = formatDate(response.data.dt*1000)
 }
 
 let apiKey = "97a9745b0c3a1f932357060a2331ab49";
